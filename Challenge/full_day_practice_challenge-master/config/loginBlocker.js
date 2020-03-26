dcca4492a7094397d5540b1aa3893758bfbb4a6a
Bashayr Alhasani <bashaier.alhassani@gmail.com>
2020-03-26 11:46:18 +0300

    third commit

--- Challenge/full_day_practice_challenge-master/config/loginBlocker.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(request, response, next) {
-  if (!request.user) {
-    request.flash("error", "You must be logged in to access that page");
-    response.redirect("/auth/login");
-  } else {
-    next();
-  }
-};
--- Challenge/full_day_practice_challenge-master/routes/auth.route.js
+++ Challenge/full_day_practice_challenge-master/routes/auth.route.js
@@ -1,6 +1,25 @@
 const router = require("express").Router();
 const passport = require("../config/passportConfig");
 const isLoggedIn = require("../config/loginBlocker");
+const User = require("../models/user.model");
+const { check, validationResult } = require("express-validator");
+
+
+router.get("/auth/signup", (request, response) => {
+  response.render("auth/signup");
+});
+
+//error decleration
+router.post("/auth/signup", [
+  check('firstName').isLength({ min: 2 }),
+  check('lastName').isLength({ min: 2 }),
+  check('phoneNumber').isLength({ min: 9 }),
+  check('password').isLength({ min: 5 }),
+  check('city').isLength({ min: 3 }),
+  check('street').isLength({ min: 5 }),
+  check('age').isLength({ min: 2 }),
+], 
+
 
 //--- Login Route 
 router.post(
@@ -13,9 +32,14 @@ router.post(
   })
 );
 
+
+
+
 //--- Logout Route
 router.get("/auth/logout", (request, response) => {
   request.logout(); //clear and break session
   request.flash("success", "Dont leave please come back!");
   response.redirect("/auth/signin");
 });
+
+module.exports = router;
